486303e56137d998a9f81d87fd696a3400ed4a33
Mirek Zielinski <mirek@parasoft.com>
2023-03-09 09:24:14 +0000

    REQ implemented

--- goo_test.cpp
+++ goo_test.cpp
@@ -11,12 +11,12 @@ TEST(GooTest, TestGetValueWithABNegative_1) {
   EXPECT_EQ(getValue(-3, -5), -3);
 }
 
-/*
+
 TEST(GooTest, TestGetValueWithPositive) {
   EXPECT_EQ(getValue(5, 5), 25);
 }
-*/
 
+ 
 
 /*
 TEST(GooTest, TestGetValueWithABNegative_2) {
