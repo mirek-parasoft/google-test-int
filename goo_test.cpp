48be961551f9a61c3c3b1d247a567f687c3bf349
Mirek Zielinski <mirek@parasoft.com>
2023-03-14 08:55:47 +0000

    Demo reverted

--- goo_test.cpp
+++ goo_test.cpp
@@ -11,9 +11,11 @@ TEST(GooTest, TestGetValueWithABNegative_1) {
   EXPECT_EQ(getValue(-3, -5), -3);
 }
 
+/*
 TEST(GooTest, TestGetValueWithPositive) {
   EXPECT_EQ(getValue(5, 5), 25);
 }
+*/
 
  
 /*
